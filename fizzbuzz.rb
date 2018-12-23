e8f341eca8925864ad64901e0ab385a856b13eb1
Taylor Swinton <taylorswinton1@gmail.com>
2018-12-23 00:01:00 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -7,11 +7,11 @@
 # we expect fizzbuzz(4) to return nil
 
 def fizzbuzz(int)
-  if int % 3 == 0
+  if int % 15 == 0
+  "FizzBuzz"
+  elsif int % 3 == 0
     "Fizz"
-  elsif int % 5  == 0
+  else int % 5  == 0
     "Buzz"
-  else int % 15 == 0
-    "FizzBuzz"
   end
 end
